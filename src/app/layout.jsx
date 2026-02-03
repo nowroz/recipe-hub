97154b73916a551b6b1b9985004e20652bf17792
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2026-02-03 07:16:43 +0600

    Set Inter as default font

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`${inter.className} antialiased`}
       >
         <header>
           <Navbar></Navbar>
